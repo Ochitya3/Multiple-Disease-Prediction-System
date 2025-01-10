7df68e848a8146318a6e64d90a7ea52f9b335779
Ochitya Singhal <116650004+Ochitya3@users.noreply.github.com>
2025-01-10 22:26:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
 import streamlit as st
-from predict_diabetes import show_predict_diabetes
-from predict_heart import show_predict_heart
+st.set_page_config(page_title="Disease Prediction", page_icon="🩺")
 
-st.set_page_config(page_title="Disease Prediction", page_icon="🩺", layout="wide")
+from home import main
 
-st.sidebar.title("Disease Prediction System")
-st.sidebar.write("Choose the disease to predict from the menu below:")
-
-menu = st.sidebar.selectbox("Select a Disease", ["Diabetes", "Heart Disease"])
-
-if menu == "Diabetes":
-    show_predict_diabetes()
-elif menu == "Heart Disease":
-    show_predict_heart()
+main()
